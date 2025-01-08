72fbeb3091374395fb3dd119a51355081b7b15c4
Sardor <sardoruchqunov@gmail.com>
2025-01-08 17:18:04 +0500

    auth

--- lib/features/auth/presentation/controllers/auth_bloc.dart
+++ lib/features/auth/presentation/controllers/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -24,23 +25,126 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     this._signOut,
     this._signUp,
   ) : super(AuthState._initial()) {
-    on<AuthInitialize>((event, emit) async {
-      final resp = await _initialize();
-      resp.fold(
-        (l) => emit(state.copyWith(error: l.msg)),
-        (r) {
-          if (r != null) {
-            emit(AuthState(
-              email: r.email,
-              name: r.name,
-              photo: r.photo,
-              status: AuthStatus.registered,
-            ));
-          } else {
-            emit(state.copyWith(status: AuthStatus.none));
-          }
-        },
-      );
-    });
+    on<AuthInitialize>(_onInit);
+    on<AuthSignIn>(_onSignIn);
+    on<AuthSignUp>(_onSignUp);
+    on<AuthSignOut>(_onSignOut);
+    on<AuthSendPasswordReset>(_onPasswordReset);
+    on<AuthSendEmailVerification>(_onVerify);
+    on<AuthConfirmPasswordReset>(_onConfirm);
+  }
+
+  FutureOr<void> _onInit(
+    AuthInitialize event,
+    Emitter<AuthState> emit,
+  ) async {
+    final resp = await _initialize();
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) {
+        if (r != null) {
+          emit(AuthState(
+            email: r.email,
+            name: r.name,
+            photo: r.photo,
+            status: AuthStatus.registered,
+          ));
+        } else {
+          emit(state.copyWith(status: AuthStatus.none));
+        }
+      },
+    );
+  }
+
+  FutureOr<void> _onSignIn(
+    AuthSignIn event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _signIn(param: (event.email, event.password));
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) {
+        emit(AuthState(
+          email: r.email,
+          name: r.name,
+          photo: r.photo,
+          status: AuthStatus.registered,
+        ));
+      },
+    );
+  }
+
+  FutureOr<void> _onSignUp(
+    AuthSignUp event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _signUp(param: (
+      event.email,
+      event.password,
+      event.name,
+    ));
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) {
+        emit(AuthState(
+          email: r.email,
+          name: r.name,
+          photo: r.photo,
+          status: AuthStatus.registered,
+        ));
+      },
+    );
+  }
+
+  FutureOr<void> _onSignOut(
+    AuthSignOut event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _signOut();
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) {
+        emit(AuthState._initial().copyWith(status: AuthStatus.none));
+      },
+    );
+  }
+
+  FutureOr<void> _onPasswordReset(
+    AuthSendPasswordReset event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _passwordReset(param: event.email);
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) => emit(state.copyWith()),
+    );
+  }
+
+  FutureOr<void> _onVerify(
+    AuthSendEmailVerification event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _emailVerification();
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) => emit(state.copyWith()),
+    );
+  }
+
+  FutureOr<void> _onConfirm(
+    AuthConfirmPasswordReset event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+    final resp = await _reset(param: (event.code, event.email));
+    resp.fold(
+      (l) => emit(state.copyWith(error: l.msg)),
+      (r) => emit(state.copyWith()),
+    );
   }
 }
